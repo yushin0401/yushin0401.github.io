dd5b601fc10d1f9725ab1752e7f0c890c2a46003
yushin0401 <79268136+yushin0401@users.noreply.github.com>
2021-04-05 11:14:13 +0900

    Create sxcript.js

---
+++ sxcript.js
@@ -0,0 +1,87 @@
+$(function() {
+  $('#hide-text').click(function(){
+    $('#text-1').show();
+    $('#hide-text').hide();
+  });
+  
+});
+
+$(function() {
+  $('#y-1').click(function(){
+    $('#text-2').show();
+    $('#text-1').hide();
+  });
+  
+});
+
+$(function() {
+  $('#n-1').click(function(){
+    $('#text-2').show();
+    $('#text-1').hide();
+  });
+  
+});
+
+$(function() {
+  $('#y-2').click(function(){
+    $('#text-3').show();
+    $('#text-2').hide();
+  });
+  
+});
+
+$(function() {
+  $('#n-2').click(function(){
+    $('#text-3').show();
+    $('#text-2').hide();
+  });
+  
+});
+
+$(function() {
+  $('#y-3').click(function(){
+    $('#text-4').show();
+    $('#text-3').hide();
+  });
+  
+});
+
+$(function() {
+  $('#n-3').click(function(){
+    $('#text-4').show();
+    $('#text-3').hide();
+  });
+  
+});
+
+$(function() {
+  $('#y-4').click(function(){
+    $('#text-5').show();
+    $('#text-4').hide();
+  });
+  
+});
+
+$(function() {
+  $('#n-4').click(function(){
+    $('#text-5').show();
+    $('#text-4').hide();
+  });
+  
+});
+
+$(function() {
+  $('#y-5').click(function(){
+    $('#text-6').show();
+    $('#text-5').hide();
+  });
+  
+});
+
+$(function() {
+  $('#n-5').click(function(){
+    $('#text-6').show();
+    $('#text-5').hide();
+  });
+  
+});
